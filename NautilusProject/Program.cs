f9189ea606d7c6b88f756361fff9ab4df6cb1531
xpn <xpn@PROMETHEUS>
2021-05-04 20:37:52 +0100

    Updated Program.cs with platform checks

--- NautilusProject/Program.cs
+++ NautilusProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace NautilusProject
 {
@@ -6,9 +7,20 @@ namespace NautilusProject
     {
         private static void Main(string[] args)
         {
-            byte[] shellcode = System.IO.File.ReadAllBytes("C:\\Users\\xpn\\Desktop\\beacon.bin");
+            Console.WriteLine("Weird Ways To Execute Unmanaged Code... by @_xpn_");
+
+            if (Environment.Version.Major != 5 || RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false || IntPtr.Size != 8)
+            {
+                Console.WriteLine("[!] Warning: This project was tested on .NET Core 5.0.5 (x64 assembly) with a Windows Host OS.");
+                Console.WriteLine("[!] Warning: If running on a different Host OS, architecture or version of .NET, your results may vary.");
+            }
+
+            var shellcode = System.IO.File.ReadAllBytes("beacon.bin");
             CombinedExec.Execute(shellcode);
-            //ExecNativeSlot.Execute(shellcode);
+
+            //ExecStubOverwrite.Execute(shellcode);
+            //ExecStubOverwriteWithoutPInvoke.Execute(shellcode);
+            //ExecNativeSlot.Execute();
         }
     }
 }
